21aad2f6d2d3dce47526c86447fdde36cb3d5198
johannesliem <liemj@science.oregonstate.edu>
2014-06-18 15:03:01 -0700

    PreProcTool OutputWorker
    Moved Output Operations from main.py to OutputWorker util

--- PreProcTool/source/l2utils/outputworker.py
+++ PreProcTool/source/l2utils/outputworker.py
@@ -1,6 +1,7 @@
 import sys, os, logging, distutils.core
 from regex import sub
 import json
+import zipfile
 
 class OutputWorker(object):
     def __init__(self, PROJECT, CONFIG):
@@ -12,16 +13,21 @@ class OutputWorker(object):
     
     def copyWebbase(self):
         try:
+            logCopyWebbase = logging.getLogger('output.copyWebbase')
+            logCopyWebbase.info('Copy webbase to output directory')
+
+            excEnv = os.path.splitext(self.CONFIG['APPLICATION']['FILE'])[1]
+            # http://stackoverflow.com/questions/15034151/copy-directory-contents-into-a-directory-with-python
             if(excEnv == '.py'):
-                distutils.dir_util.copy_tree("..\..\WebVisTool\\build\dist", CONFIG['PROJECT']['OUTPUT_DIR'])
+                distutils.dir_util.copy_tree("..\..\WebVisTool\\build\dist", self.CONFIG['PROJECT']['OUTPUT_DIR'])
             if(excEnv == '.exe'):
-                distutils.dir_util.copy_tree(appPath + "\webbase", CONFIG['PROJECT']['OUTPUT_DIR'])
+                distutils.dir_util.copy_tree(appPath + "\webbase", self.ONFIG['PROJECT']['OUTPUT_DIR'])
         except Exception as e:
-            logGenOutputDir.error('{}'.format(e))
+            logCopyWebbase.error('{}'.format(e))
 
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            logGenOutputDir.debug('{}::{}::{}'.format(exc_type, fname, exc_tb.tb_lineno))
+            logCopyWebbase.debug('{}::{}::{}'.format(exc_type, fname, exc_tb.tb_lineno))
 
             sys.exit()   
 
@@ -93,25 +99,83 @@ class OutputWorker(object):
 
     def saveMetadataJson(self):
         try:
-            # print os.path.normpath(CONFIG['PROJECT']['OUTPUT_DIR']+'/landisdata/metadata/metadata.scenarios.json')
+            logSaveMetadata = logging.getLogger('output.saveMetadata')
+            logSaveMetadata.info('Save Metadata as JSON files')
+
             scenariosDictToJson = {}
-            scenariosDictToJson['scenarios'] = PROJECT.getScenarioDict()
+            scenariosDictToJson['scenarios'] = self.PROJECT.getScenarioDict()
             j = json.dumps(scenariosDictToJson, sort_keys=True, indent=2)
-            f = open(os.path.normpath(CONFIG['PROJECT']['OUTPUT_DIR']+'/landisdata/metadata/metadata.scenarios.json'), 'w')
+            f = open(os.path.normpath(self.CONFIG['PROJECT']['OUTPUT_DIR']+'/landisdata/metadata/metadata.scenarios.json'), 'w')
             print >> f, j
             f.close()
 
-            #print os.path.normpath(CONFIG['PROJECT']['OUTPUT_DIR']+'/landisdata/metadata/metadata.extensions.json')
-            extensionOutputDictToJson = PROJECT.getExtensionOutputDict()
+            extensionOutputDictToJson = self.PROJECT.getExtensionOutputDict()
             j = json.dumps(extensionOutputDictToJson, sort_keys=True, indent=2)
-            f = open(os.path.normpath(CONFIG['PROJECT']['OUTPUT_DIR']+'/landisdata/metadata/metadata.extensions.json'), 'w')
+            f = open(os.path.normpath(self.CONFIG['PROJECT']['OUTPUT_DIR']+'/landisdata/metadata/metadata.extensions.json'), 'w')
             print >> f, j
             f.close()
         except Exception as e:
-            logGenOutputDir.error('{}'.format(e))
+            logSaveMetadata.error('{}'.format(e))
 
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            logGenOutputDir.debug('{}::{}::{}'.format(exc_type, fname, exc_tb.tb_lineno))
+            logSaveMetadata.debug('{}::{}::{}'.format(exc_type, fname, exc_tb.tb_lineno))
+
+            sys.exit()
+
+    def updateWebsettings(self):
+        try:
+            logUpdateWebPrefs = logging.getLogger('output.updateWebPrefs')
+            logUpdateWebPrefs.info('Update web settings file')
+            with open(os.path.normpath(self.CONFIG['PROJECT']['OUTPUT_DIR']+"/config/default_settings.json"), "r+") as jsonFile:
+                data = json.load(jsonFile)
+
+                data["projectname"] = self.PROJECT.projectName
+                data["map"]["resolutions"] = self.PROJECT.getResolutions()
+                data["map"]["resolution"] = self.PROJECT.getInitResolution()
+                data["map"]["basemap"]["saturation"] = self.PROJECT.initSaturation
+                data["map"]["basemap"]["contrast"] = self.PROJECT.initContrast
+                data["map"]["basemap"]["brightness"] = self.PROJECT.initBrightness
+                data["map"]["basemap"]["source"] = self.PROJECT.mapSource
+
+                jsonFile.seek(0)  # rewind
+                jsonFile.write(json.dumps(data, sort_keys=False, indent=2))
+                jsonFile.truncate()
+        except Exception as e:
+            logUpdateWebPrefs.error('{}'.format(e))
+
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logUpdateWebPrefs.debug('{}::{}::{}'.format(exc_type, fname, exc_tb.tb_lineno))
+
+            sys.exit()
+
+    def zipdir(self, path, zip):
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if os.path.splitext(file)[1] != '.exe':
+                    zip.write(os.path.join(root, file))
+
+    def zipOutputDir(self):
+        try:
+            logZip = logging.getLogger('output.zipOutput')
+            logZip.info('Create zipfile for server upload')
+            # Create the zip file for server upload
+            zipdn = os.path.normpath(self.CONFIG['PROJECT']['OUTPUT_DIR'])
+            zipfn = zipdn + '.zip'
+
+            zipf = zipfile.ZipFile(zipfn, 'w')
+            self.zipdir(zipdn, zipf)
+            zipf.close()
+
+            logZip.info('Output Folder: ' + os.path.abspath(zipdn))
+            logZip.info('Zip File: ' + os.path.abspath(zipfn))
+
+        except Exception as e:
+            logZip.error('{}'.format(e))
+
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logZip.debug('{}::{}::{}'.format(exc_type, fname, exc_tb.tb_lineno))
 
             sys.exit()
--- PreProcTool/source/main.py
+++ PreProcTool/source/main.py
@@ -9,8 +9,6 @@ import logging
 import argparse
 import l2data as landis, l2utils as utils
 import time
-import json
-import zipfile
 
 from multiprocessing import freeze_support
 
@@ -50,26 +48,15 @@ def parseArguemnts():
 
     return parser.parse_args()
 
-def zipdir(path, zip):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if os.path.splitext(file)[1] != '.exe':
-                zip.write(os.path.join(root, file))
-
 def main(script, *args):
     try:
         # get the path and the file of the application file (executeable)
         appPath, appFile = os.path.split(os.path.normpath(os.path.realpath(script)))
 
-        # setup the logging system
-        # if not os.path.isdir(appPath + '    \logs'):
-        #     os.mkdir(appPath +'\logs')
-        # elif os.path.exists(appPath+'\logs\preproctool.log'):
-        #     os.remove(appPath+'\logs\preproctool.log')
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s %(name)-25s %(levelname)-8s %(message)s',
                             datefmt='%m/%d/%Y %I:%M:%S %p',
-                            filename=appPath+'\logs\preproctool.log',
+                            filename=appPath+'\logs\\' + time.strftime('%Y%m%d-%H%M%S') + '.log',
                             filemode = 'w')
 
         console = logging.StreamHandler()
@@ -90,18 +77,13 @@ def main(script, *args):
         # PROJECT stores the complete Project (Infos aboute the Project/Scenarios/Extensions/Outputs)
         PROJECT = collector.setupProject()
         
-        # FIXME
-        # LANDIS PreProc Checker: controlles the Project
-        # rules: what should the checker check?
-        # checker = landis.Checker()
-
         # LANDIS Preworker: prepair Tables and Maps
         preworker = utils.Preworker(PROJECT, CONFIG)
 
         outputworker = utils.OutputWorker(PROJECT, CONFIG)
         outputworker.generateOutputDirs()
 
-  
+        # FIXME move timing stuff to preworker ...
         logMain.info('Start prepairing Table Output')
         start = time.time()
         preworker.prepairTables()
@@ -114,46 +96,10 @@ def main(script, *args):
         end = time.time()
         logMain.info('End prepairing Map Output [time: {} sec]'.format(end - start))
 
-        
-        # FIXME: add to new util: webworker.py
-
         outputworker.saveMetadataJson()
-
-        # FIXME WebWorker:
-        # NOT FOR DIST, JUST FOR DEVEL; FOR DIST WEBBASE FROM APP PATH
-        # http://stackoverflow.com/questions/15034151/copy-directory-contents-into-a-directory-with-python
-        outputworker.copyWebbase(os.path.splitext(appFile)[1])
-
-        
-
-        with open(os.path.normpath(CONFIG['PROJECT']['OUTPUT_DIR']+"/config/default_settings.json"), "r+") as jsonFile:
-            data = json.load(jsonFile)
-
-            data["projectname"] = PROJECT.projectName
-            data["map"]["resolutions"] = PROJECT.getResolutions()
-            data["map"]["resolution"] = PROJECT.getInitResolution()
-            data["map"]["basemap"]["saturation"] = PROJECT.initSaturation
-            data["map"]["basemap"]["contrast"] = PROJECT.initContrast
-            data["map"]["basemap"]["brightness"] = PROJECT.initBrightness
-            data["map"]["basemap"]["source"] = PROJECT.mapSource
-
-            jsonFile.seek(0)  # rewind
-            jsonFile.write(json.dumps(data, sort_keys=False, indent=2))
-            jsonFile.truncate()
-
-        # Create the zip file for server upload
-        zipdn = os.path.normpath(CONFIG['PROJECT']['OUTPUT_DIR'])
-        zipfn = zipdn + '.zip'
-        logging.info('Create zipfile for server upload')
-
-        zipf = zipfile.ZipFile(zipfn, 'w')
-        zipdir(zipdn, zipf)
-        zipf.close()
-        
-
-        logging.info('Output Folder: ' + os.path.abspath(zipdn))
-        logging.info('Zip File: ' + os.path.abspath(zipfn))
-        # End WEBWORKER
+        outputworker.copyWebbase()
+        outputworker.updateWebsettings()
+        outputworker.zipOutputDir()
 
         logMain.info('End of LANDIS-II PreProcTool.')
 
