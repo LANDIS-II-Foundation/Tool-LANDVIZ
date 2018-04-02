0f968a23c12f99d1b56063ac48922698dddc3292
Makiko Shukunobe <m_shukunobe@hotmail.com>
2018-04-02 13:00:49 -0400

    example .bat files updates

--- example/example_project/run_preproctool_example.bat
+++ example/example_project/run_preproctool_example.bat
@@ -1,6 +1,5 @@
 @echo off
-cd "..\..\deploy\dist\preproctool\"
-preproctool.exe -p ..\..\..\example\example_project\preproc_VizTool_example.xml -o ..\..\..\exmaple\example_project\sample_output %*
+preproctool.exe -p preproc_VizTool_example.xml -o LANDVIZ_Example %*
 pause
 
  
